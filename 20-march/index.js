e553af520753f4f4bb2b99125ffcd66571eab8f2
vishal0930 <vishal09121998@gmail.com>
2022-03-20 16:14:45 +0530

    Assingment of 13 to 20 march

---
+++ 20-march/index.js
@@ -0,0 +1,58 @@
+// Assingment 1
+
+/*
+function sum(n1, n2) {
+    let sumOfNumbers = n1 + n2
+    // console.log(sumOfNumbers)
+    return sumOfNumbers
+}
+res = sum(9, 12)
+console.log(res)
+
+function multiple(n1, n2) {
+    let multiplyOfNumbers = n1 * n2
+    // console.log(multiplyOfNumbers)
+    return multiplyOfNumbers
+}
+res  = multiple(9, 12)
+console.log(res)
+
+function substraction(n1, n2) {
+    let substractionOfNumbers = n1 - n2
+    // console.log(substractionOfNumbers)
+    return substractionOfNumbers
+}
+res = substraction(9, 12)
+console.log(res)
+
+function division(n1, n2) {
+    let divisionOfNumbers = n1 / n2
+    // console.log(divionOfNumbers)
+    return divisionOfNumbers
+}
+res = division(12, 3)
+console.log(res)
+*/
+
+// Assingment 2
+/*
+function algebra(a, b) {
+    let count = ((a*a) + (2*a*b) + (b*b))
+    // console.log(count)
+    return count
+}
+res = algebra(12, 9)
+console.log(res)
+*/
+
+// Assingment 3
+
+var a=0, b=1, c;
+console.log(a)
+console.log(b)
+for (let i = 3; i<=10; i++) {
+    c = a+b
+    console.log(c)
+    a = b
+    b = c    
+}
